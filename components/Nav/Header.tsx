784233918bc60a3bfd47228ad097048876ea87e4
Isibor Jerry <47747113+saturnarians@users.noreply.github.com>
2025-10-16 09:40:27 +0100

    Update Header.tsx

--- components/Nav/Header.tsx
+++ components/Nav/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { House, Image, Video, Brush, WandSparkles, Folder, DraftingCompass } from 'lucide-react';
-import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useState, useEffect} from 'react';
 
